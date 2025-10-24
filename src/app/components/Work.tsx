5b3153ed8b2f342e4e47d8655aba71993c10f869
Nishtha Pant <nishthapant@Nishthas-MacBook-Pro.local>
2025-10-24 12:15:04 -0600

    added favicon

--- src/app/components/Work.tsx
+++ src/app/components/Work.tsx
@@ -6,7 +6,9 @@ import WorkList from "./WorkList";
 const Work = () => {
   return (
     <div className="max-w-3xl flex flex-col gap-2 work">
-      <div className="section-heading text-xl md:text-3xl">Work Experience</div>
+      <div className="section-heading text-2xl text-center md:text-3xl md:text-start">
+        Work Experience
+      </div>
       <WorkList />
     </div>
   );
